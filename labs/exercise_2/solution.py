967da4c0912c949e562ddb5232c1ced2f4c31c86
teossaurus <teo.a.soares@gmail.com>
2015-02-18 23:10:27 -0500

    Changes to be committed: 	modified:   solution.py

--- labs/exercise_2/solution.py
+++ labs/exercise_2/solution.py
@@ -1,19 +1,10 @@
 fibo = [1]
 evens = []
 
-for n in fibo:
-#adds n to its predecessor in the list to get i
+for n in fibo if not n => 4000000:
 	i = n + fibo[fibo.index(n)-1]
-#populates fibo with i up to four million so the loop can continue
 	if i <= 4000000:
 		fibo.append(i)
-#populates evens with the even numbers in the Fibonacci sequence
-	if i % 2 == 0 and i <= 4000000:
-		evens.append(i)
 
-print evens
-print sum(evens)
-
-
-
-
+print [i for i in fibo if not i % 2]
+print sum(i for i in fibo if not i % 2)

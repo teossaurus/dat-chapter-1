20ccae45f671208fe29bb6316227876e17371ba8
teossaurus <teo.a.soares@gmail.com>
2015-02-22 22:36:24 -0500

    solution.py updated

--- labs/exercise_2/solution.py
+++ labs/exercise_2/solution.py
@@ -1,9 +1,8 @@
 fibo = [1]
 
-for n in fibo if not n => 4000000:
+for n in fibo:
 	i = n + fibo[fibo.index(n)-1]
 	if i <= 4000000:
 		fibo.append(i)
 
-print [i for i in fibo if not i % 2]
 print sum(i for i in fibo if not i % 2)
